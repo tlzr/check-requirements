7bde7593226c5be44ddd970e69ac02bacb3c760e
Ivan I <tlzr@mymailbox.pp.ua>
2014-10-06 18:10:37 +0300

    Add arguments processment

--- caheck.py
+++ caheck.py
@@ -2,7 +2,7 @@
 """
 USAGE:
 
-caheck.py ssh://path/to/the/repo
+caheck.py [projects]
 
 This script clones repository which is not exists and
 after that it checks requirements.txt for any changes.
@@ -11,38 +11,83 @@ Please ensure that GitPython is installed:
 pip install GitPython
 """
 
+import argparse
 import git
 import os
+import StringIO
 import sys
 
-#knownBuilds = ('python-novaclient', 'nova')
 gitloc = 'https://github.com/openstack/'
 repodir = 'cache'
 reqfile = 'requirements.txt'
 logformat = 'format:%H,%at'
 
 def check_args():
-    if not len(sys.argv) > 1:
-        print __doc__
-        sys.exit(0)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--save', metavar=('PROJECT', 'HASH'), nargs=2, help='Save the commit')
+    parser.add_argument('-sl', '--save-last', metavar=('PROJECT'), nargs=1, help='Save the last commit')
+    parser.add_argument('projects', nargs='+', help='List of projects, e.g. nova or python-novaclient')
 
-def check_path(name):
+    try:
+        args = parser.parse_args()
+    except Exception as e:
+        parser.print_help()
+
+    return args
+
+def process(name):
     repoloc = repodir + '/' + name
 
     if os.path.exists(repoloc):
         repo = git.Git(repoloc)
-        repo.pull()
-        repo.log('--since=1410993264', '--pretty=' + logformat, reqfile)
-        repo.config('remote.origin.url')
+
+        logs = StringIO.StringIO(repo.log('--since=1410993264', '--pretty=' + logformat, reqfile))
+        for i in logs:
+            commit_hash, commit_unixtime = i.split(',')
+            print commit_hash
+            print commit_unixtime
+                       
+        #piz2 = repo.config('remote.origin.url')
+        #print piz2
     else:
         git_clone(name, repoloc)
 
+def git_pull(name, repoloc):
+    repo = git.Git(repoloc)
+    print 'Pulling %s ...' % (name)
+    repo.pull()
+
 def git_clone(name, repoloc):
-    print 'Clonning %s into %s ...' repoloc + '.git', repodir + name
+    print 'Clonning %s into %s ...' % (repoloc + '.git', repodir + '/' + name)
     git.Git().clone(gitloc + name + '.git', repoloc)
 
+def pro(name):
+    print name
+
 if __name__ == "__main__":
-    check_args()
-    knownBuilds = sys.argv[1:]
-    [check_path(i) for i in knownBuilds]
+    args = check_args()
+    print args
+    sys.exit(0)
+    #[pro(i) for i in parser.projects]
+    #main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--save', dest='save', metavar=('PROJECT', 'HASH'), nargs=2, help='Save the commit')
+    parser.add_argument('-sl', '--save-last', dest='save_last', metavar=('PROJECT'), nargs=1, help='Save the last commit')
+    parser.add_argument('projects', nargs='+', help='List of projects, e.g. nova or python-novaclient')
+
+    try:
+        args = parser.parse_args()
+    except Exception as e:
+        parser.print_help()
+
+    print args
+ 
+    if args.projects:
+        print args.projects
+        [pro(i) for i in args.projects]
+    elif args.save:
+        print 'save'
+    elif args.save_last:
+        print 'save_last'
+
 
