6021437d569b949cb74bcefbdbc83d9d2776a196
Ivan I <tlzr@mymailbox.pp.ua>
2014-10-07 17:40:58 +0300

    Fixed bugs

--- check.py
+++ check.py
@@ -2,7 +2,7 @@
 """
 USAGE:
 
-caheck.py [projects]
+check.py -h
 
 This script clones repository which doesn't exist and
 pulls requirements.txt
@@ -16,6 +16,7 @@ pip install GitPython
 import argparse
 import git
 import os
+import re
 import StringIO
 import sys
 
@@ -24,13 +25,19 @@ repodir = 'cache'
 reqfile = 'requirements.txt'
 logformat = 'format:%H,%at'
 
+hashre = re.compile('^[a-z0-9]+$')
+unixtimere = re.compile('^[0-9]+$')
+
+
 def check_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-al',, '--all-logs', action='store_true', help='Get all logs')
-    parser.add_argument('-df',, '--diff', action='store_true', help='Get diff')
-    parser.add_argument('-l', '--list', metavar=('PROJECT'), nargs=1, help='Get commits list')
-    parser.add_argument('projects', nargs='*', help='List of projects, e.g. nova or python-novaclient')
+    parser.add_argument('-al', '--all-logs', metavar=('PROJECT'), nargs='+', help='Get all logs')
+    parser.add_argument('-df', '--diff', metavar=('PROJECT'), nargs='+', help='Get diff')
     parser.add_argument('-sl', '--save-last', metavar=('PROJECT'), nargs=1, help='Save the last commit')
+    parser.add_argument('-u', '--update', metavar=('PROJECT'), nargs='+', help='Update project list')
+
+    if not len(sys.argv) > 1:
+        parser.print_help()
 
     try:
         args = parser.parse_args()
@@ -40,7 +47,7 @@ def check_args():
     return args
 
 def get_repodir(name):
-    repoloc = os.path.join(repodir, '/', name)
+    repoloc = os.path.join(repodir, name)
 
     return repoloc
 
@@ -69,34 +76,43 @@ def git_log(name):
     repoloc = get_repodir(name)
     fpath = repoloc + '.txt'
 
-    if git_check_checkpoint(fpath) and not args.all-logs and not args.save-last:
-        commit_hash, commit_unixtime = git_get_checkpoint()
-        logs = StringIO.StringIO(repo.log('--since' + commit_unixtime, '--pretty=' + logformat, reqfile))
+    if git_check_checkpoint(fpath) and not args.all_logs:
+        commit_hash, commit_unixtime = git_get_checkpoint(fpath)
+        logs = StringIO.StringIO(repo.log('--since=' + commit_unixtime, '--pretty=' + logformat, reqfile))
     else:
         logs = StringIO.StringIO(repo.log('--pretty=' + logformat, reqfile))
 
-    if args.all-logs:
+    if args.all_logs:
+         print '\n----------------------\nShowing logs for %s:\
+                \n----------------------' % (name)
          for i in logs:
              print i
     
-    if args.save-last:
+    if args.save_last:
         line = logs.readline()
+        print '\n----------------------\n Saving:\n         %s\
+               \n----------------------' % (line)
         git_set_checkpoint(fpath, line)
 
     if args.diff and git_check_checkpoint(fpath):
-        commit_hash, commit_unixtime = git_get_checkpoint(fpath).split(',')
+        commit_hash, commit_unixtime = git_get_checkpoint(fpath)
         upstream_commit_hash, upstream_commit_unixtime = logs.readline().split(',')
         commit_diff = repo.diff(commit_hash+'..'+upstream_commit_hash, reqfile)
-
         if commit_diff:
-            print 'Please update spec file'
-
-            if args.diff:
-                print commit_diff
+            print commit_diff
 
 def git_check_checkpoint(fpath):
     if os.path.isfile(fpath) and os.access(fpath, os.R_OK) and os.access(fpath, os.W_OK):
-        return True
+        with open(fpath, 'r') as f:
+            buf = f.readline()
+            if buf:
+                buf = buf.split(',')
+                if len(buf) == 2:
+                    hash, unixtime = buf
+                    if hashre.match(hash) and unixtimere.match(unixtime) is not None:
+                        return True
+                    else:
+                        return False
     else:
         return False
 
@@ -111,19 +127,14 @@ def git_get_checkpoint(fpath):
 
     return (commit_hash, commit_unixtime)
 
-def pro(name):
-    print name
-
 if __name__ == "__main__":
     args = check_args()
 
-    if args.projects:
-        print 'arg+proj'
-        [pro(i) for i in args.projects]
-    if args.list:
-        print 'arg+list'
-        [git_log(i) for i in args.list]
-    if args.save:
-        print 'save'
+    if args.update:
+        [git_pull(i) for i in args.update]
+    if args.all_logs:
+        [git_log(i) for i in args.all_logs]
+    if args.diff:
+        [git_log(i) for i in args.diff]
     if args.save_last:
-        print 'save_last'
+        [git_log(i) for i in args.save_last]
